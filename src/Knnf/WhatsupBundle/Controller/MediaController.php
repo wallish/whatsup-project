f8d58143348b63b7411b9ccf22ce756d3abce509
Wallish <wallish@Nirundthan-Parameswaran.local>
2014-03-20 10:47:41 +0100

    add media with crud

---
+++ src/Knnf/WhatsupBundle/Controller/MediaController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Knnf\WhatsupBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Knnf\WhatsupBundle\Entity\Media;
+use Knnf\WhatsupBundle\Form\MediaType;
+
+/**
+ * Media controller.
+ *
+ */
+class MediaController extends Controller
+{
+
+    /**
+     * Lists all Media entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('KnnfWhatsupBundle:Media')->findAll();
+
+        return $this->render('KnnfWhatsupBundle:Media:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Media entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Media();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('media_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('KnnfWhatsupBundle:Media:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to create a Media entity.
+    *
+    * @param Media $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createCreateForm(Media $entity)
+    {
+        $form = $this->createForm(new MediaType(), $entity, array(
+            'action' => $this->generateUrl('media_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Media entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Media();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('KnnfWhatsupBundle:Media:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Media entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('KnnfWhatsupBundle:Media')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Media entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('KnnfWhatsupBundle:Media:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Media entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('KnnfWhatsupBundle:Media')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Media entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('KnnfWhatsupBundle:Media:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Media entity.
+    *
+    * @param Media $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Media $entity)
+    {
+        $form = $this->createForm(new MediaType(), $entity, array(
+            'action' => $this->generateUrl('media_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Media entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('KnnfWhatsupBundle:Media')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Media entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('media_edit', array('id' => $id)));
+        }
+
+        return $this->render('KnnfWhatsupBundle:Media:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Media entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('KnnfWhatsupBundle:Media')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Media entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('media'));
+    }
+
+    /**
+     * Creates a form to delete a Media entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('media_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Knnf/WhatsupBundle/Form/MediaType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Knnf\WhatsupBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MediaType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('name')
+            ->add('title')
+            ->add('file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Knnf\WhatsupBundle\Entity\Media'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'knnf_whatsupbundle_media';
+    }
+}
